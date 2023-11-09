0657f049087194c821cdb83c10fac3cda177e0c1
Eliezer Chirino <emchch3001@gmail.com>
2023-11-09 11:48:01 -0300

    Fix(route.ts): add dynamic url redirect

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -15,5 +15,5 @@ export async function GET(request: NextRequest) {
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect('https://x-clone-em.vercel.app');
+  return NextResponse.redirect(requestUrl.origin);
 }
